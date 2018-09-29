8f00b08e7c5230f8bb889ce1f752e33d8c450dd6
root <yinthewater@gmail.com>
2018-09-29 15:32:16 -0700

    Render cards in hand

--- Assets/Fight/HandRenderer.cs
+++ Assets/Fight/HandRenderer.cs
@@ -8,27 +8,7 @@ public class HandRenderer : MonoBehaviour
 
     void Awake()
     {
-        placeholder = GameObject.FindGameObjectsWithTag("Placeholder").Single(o => o.name == "Hand");
+		placeholder = this.gameObject;
     }
 
-	public void OnCardChanges(List<Card> cards) {
-
-		var basePos = this.transform.position;
-		var hScale = this.transform.localScale;
-		var hWidth = hScale.x;
-		var hHeight = hScale.y;
-		basePos -= new Vector3(hWidth / 2, 0);
-		var scale = new Vector3(hWidth / 64, hHeight / 8, 0);
-
-		var handSize = this.GetComponent<SpriteRenderer> ().bounds.size;
-		var cardHeight = handSize.y * 0.7f;
-		var cardWidth = cardHeight * 0.618f;
-		var cardSize = new Vector2 (cardWidth, cardHeight);
-
-		for (int i = 0; i < cards.Count; i++) 
-		{
-			var pos = basePos + new Vector3(i * hWidth / 12 + hWidth / 24, 0, 1);
-			cards[i].Instantiate (this.placeholder, pos, cardSize);
-		}
-	}
 }
--- Assets/FightScene/Hand.cs
+++ Assets/FightScene/Hand.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class Hand {
+public class HandScript : MonoBehaviour {
 
     GameObject handObject;
 
@@ -16,53 +16,27 @@ public class Hand {
 
     public List<Card> cards;
 
-    public Hand(FightScene fightScene)
-    {
-        this.FightScene = fightScene;
-
-        handObject = GameObject.FindGameObjectsWithTag("Placeholder").Single(o => o.name == "Hand");
-        handObject.AddComponent<HandRenderer>();
-
+	public void Awake() {
+        this.FightScene = Camera.main.GetComponent<FightScene>();
+		this.handObject = this.gameObject;
 		this.States = new StatesBar (handObject);
-		this.drawPerTurn = FightScene.GameStats.GetSpeed();
-    }
-
-	public void Render()
-	{
-		var basePos = handObject.transform.position;
-		var hScale = handObject.transform.localScale;
-		var hWidth = hScale.x;
-		var hHeight = hScale.y;
-		basePos -= new Vector3(hWidth / 2, 0);
-		var scale = new Vector3(hWidth / 64, hHeight / 8, 0);
-
-		var handSize = handObject.GetComponent<SpriteRenderer> ().bounds.size;
-		var cardHeight = handSize.y * 0.7f;
-		var cardWidth = cardHeight * 0.618f;
-		var cardSize = new Vector2 (cardWidth, cardHeight);
-
-		for (int i = 0; i < cards.Count; i++) 
-		{
-			var pos = basePos + new Vector3(i * hWidth / 12 + hWidth / 24, 0, 1);
-			//cards[i].Render (handObject, pos, cardSize);
-		}
+		this.drawPerTurn = this.FightScene.GameStats.GetSpeed();
 	}
 
 	public void DrawNewCard(int num)
 	{
-		if(this.States.AllowDraw == true){
+		if(this.States.AllowDraw == true) {
 			for (int i = 0; i < num; i++)
 			{
-				/*
-				var newCard = //this.FightScene.DrawPile.Draw();
+				var newCard = new Card();
+				this.FightScene.BroadcastMessage("OnDrawingCard", newCard);
 				if (newCard == null) {
 					break;
 				}
 				cards.Add(newCard);
-				newCard.FightScene = this.FightScene;
-				*/
+				Debug.Log("drew card" + newCard.Meta.Name);
 			}
-			this.Render ();
+			//this.Render ();
 		}
 	}
 
@@ -75,23 +49,45 @@ public class Hand {
 				cards.Remove (card);
 			}
 		}
-		this.Render ();
+		//this.Render ();
 	}
 
-    public void StartTurn()
-    {
+	public void OnTurnStart(int turnCounter)
+	{
         cards = new List<Card>();
 		this.States.StartTurn ();
 		var numToDraw = drawPerTurn + this.States.ExtraCardsNum;
 		DrawNewCard(numToDraw);
-    }
+		OnHandChanges();
+	}
 
-    public void EndTurn()
-    {
+	public void OnTurnEnd(int turnCounter)
+	{
 		this.States.EndTurn ();
 		foreach(var card in cards)
         {
 			//card.PassiveDiscard ();
         }
     }
+
+	public void OnHandChanges() {
+
+		var basePos = this.transform.position;
+		var hScale = this.transform.localScale;
+		var hWidth = hScale.x;
+		var hHeight = hScale.y;
+		basePos -= new Vector3(hWidth / 2, 0);
+		var scale = new Vector3(hWidth / 64, hHeight / 8, 0);
+
+		var handSize = this.GetComponent<SpriteRenderer> ().bounds.size;
+		var cardHeight = handSize.y * 0.7f;
+		var cardWidth = cardHeight * 0.618f;
+		var cardSize = new Vector2 (cardWidth, cardHeight);
+
+		for (int i = 0; i < cards.Count; i++) 
+		{
+			var pos = basePos + new Vector3(i * hWidth / 12 + hWidth / 24, 0, 1);
+			cards[i].Instantiate (this.gameObject, pos, cardSize);
+		}
+	}
 }
