617c8b7a7aec57cbd9d0935c73a8920618d6fe88
root <yinthewater@gmail.com>
2018-09-29 23:03:14 -0700

    enable display comments

--- Assets/Cards/Card.cs
+++ Assets/Cards/Card.cs
@@ -9,7 +9,7 @@ public class Card {
 		go.transform.SetParent(parent.transform, false);
 		go.AddComponent<SpriteRenderer>().sprite = Resources.Load<Sprite>(this.Meta.SpriteName);
 		go.AddComponent<BoxCollider2D>();
-		go.AddComponent<CardRenderer>();
+		go.AddComponent<CardRenderer>().Meta = Meta;
 
 		go.transform.position = pos;
 		go.GetComponent<SpriteRenderer>().sortingOrder = 1;
@@ -23,9 +23,41 @@ public class Card {
 
 public class CardRenderer : MonoBehaviour {
 
+	TextObject cardDescription;
+	public CardMeta Meta;
+
+
 	public void Awake() {
 	}
 
 	public void Start() {
+	}
+
+	void OnMouseOver() {
+		displayCardDescription();
+	}
+
+	void OnMouseExit() {
+		hideCardDescription();
+	}
+
+	void displayCardDescription() {
+		if(this.cardDescription != null) {
+			return;
+		}
+
+		this.cardDescription = new TextObject(this.gameObject);
+
+		this.cardDescription.Renderer.TextComponent.text = Meta.Name + ": \n" + Meta.Comment;
+		this.cardDescription.Renderer.TextComponent.color = Color.blue;
+		this.cardDescription.Renderer.TextComponent.alignment = TextAnchor.MiddleCenter;
+		this.cardDescription.Renderer.TextComponent.fontSize = 20;
+	}
+
+	void hideCardDescription() {
+		if(this.cardDescription != null) {
+			this.cardDescription.Destroy();
+			this.cardDescription = null;
+		}
 	}
 }
