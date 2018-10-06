5fc4f1e3c8c6f7639744ed4633911ec37ac29ac9
Han Yin <yinthewater@gmail.com>
2018-10-06 16:10:02 -0700

    play cards and deal damage to monsters!

--- Assets/Fight/Effect.cs
+++ Assets/Fight/Effect.cs
@@ -8,19 +8,36 @@ public class FightData {
 	public List<GameObject> Opponents;
 }
 
+public enum EffectType {
+	ApplyDamage,
+	ApplyVulnerable,
+}
+
+// Effects message that will be broadcasted
+public class EffectMsg {
+	// root transform of who emit this effect
+	// all other objects would be the effect taker
+	public GameObject Emitter;
+
+	// broadcastMessage key
+	public string MethodName() {
+		return "On" + ActiveEffect.Type.ToString();
+	}
+
+	public Effect ActiveEffect;
+}
+
+// Effects should be immutable
 public class Effect {
-}
+	public EffectType Type;
 
-public class DamageEffect : Effect{
-	public int Damage;
-}
-
-public class VulnerableEffect : Effect{
 	public int Duration;
-}
+	public int Value;
 
-public class SelfDamageEffect : Effect{
-}
-
-public class PercentageDamageEffect : Effect{
+	public EffectMsg GenerateMsg(GameObject emitter) {
+		return new EffectMsg() {
+			Emitter = emitter,
+			ActiveEffect = this,
+		};
+	}
 }
