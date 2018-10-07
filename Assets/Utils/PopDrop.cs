e0fad38b0accff2501e03399bbf28adc4be68568
Han Yin <yinthewater@gmail.com>
2018-10-06 17:01:33 -0700

    remove duplicated pop drop

--- Assets/Utils/PopDrop.cs
+++ Assets/Utils/PopDrop.cs
@@ -25,20 +25,27 @@ public class PopDropRenderer : MonoBehaviour
 {
 	Rigidbody2D body;
 	float duration = 3;
+	float defer;
 
 	public void Awake() {
 		this.body = this.gameObject.AddComponent<Rigidbody2D> ();
 
 		this.body.velocity = new Vector2 (Random.Range (-150, 150), 400);
 		this.body.gravityScale = 125;
+
+		this.defer = Random.Range(0.0f, 1.5f);
 	}
 
 	public void Update() {
+		if (this.defer > 0) {
+			this.defer -= Time.deltaTime;
+			return;
+		}
+
 		if (this.duration < 0) {
 			Destroy (this.gameObject);
 		}
 		this.duration -= Time.deltaTime;
-
 
 		var newScale = Mathf.Lerp(1.5f, 0.1f, Mathf.Abs(2.7f - duration));
 		transform.localScale = new Vector3(newScale, newScale, 1);
